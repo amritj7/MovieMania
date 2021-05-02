536328b3be0bace964e5f3390f181d64b45fb957
Amritansh Dixit <amritansh7757@gmail.com>
2021-05-02 13:06:11 +0530

    render serached movies

--- app.py
+++ app.py
@@ -1,50 +1,53 @@
-import requests, json
+import requests
+import json
 
 from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mydb"
-mongo = PyMongo(app)
-db = mongodb_client.db
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mydb"
+# mongo = PyMongo(app)
+# db = mongodb_client.db
+
 
 @app.route('/search/<name>', methods=['POST', 'GET'])
 def search(name):
-	url = "https://imdb8.p.rapidapi.com/auto-complete"
-	querystring = {"q":name}
-	headers = {
-		'x-rapidapi-key': "319ebe4cf1msha602967cc60d6c6p1cafdbjsn7078064544f6",
-		'x-rapidapi-host': "imdb8.p.rapidapi.com"
-	}
-	response = requests.request("GET", url, headers=headers, params=querystring)
-	return json.dumps(response.json()['d'][:min(5,len(response.json()['d']))])
+    url = "https://imdb8.p.rapidapi.com/auto-complete"
+    querystring = {"q": name}
+    headers = {
+        'x-rapidapi-key': "319ebe4cf1msha602967cc60d6c6p1cafdbjsn7078064544f6",
+        'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
+    return json.dumps(response.json()['d'][:5])
 
-@app.route('/comment', methods=['POST', 'GET'])
-def comment():
-	data = request.get_json()
-	foundInMovies = mongo.db.movie.find_one({'id' : data.movieID})
-	user = mongo.db.movie.find_one({'userID' : data.userID})
-	if not data.movieID in user.movies :
-		user.movies.append(data.movieID)
-	if foundInMovies :
-		foundInMovies.comments.append(data.comment)
-	else :
-		mongo.db.movie.insert_one({'id' : data.movieID, 'rating' : {0, 0}, 'comments' : [{data.comment}]})	
-    return mongo.db.movie.find_one({'id' : data.movieID})
+# @app.route('/comment', methods=['POST', 'GET'])
+# def comment():
+# 	data = request.get_json()
+# 	foundInMovies = mongo.db.movie.find_one({'id' : data.movieID})
+# 	user = mongo.db.movie.find_one({'userID' : data.userID})
+# 	if not data.movieID in user.movies :
+# 		user.movies.append(data.movieID)
+# 	if foundInMovies :
+# 		foundInMovies.comments.append(data.comment)
+# 	else :
+# 		mongo.db.movie.insert_one({'id' : data.movieID, 'rating' : {0, 0}, 'comments' : [{data.comment}]})
+#     return mongo.db.movie.find_one({'id' : data.movieID})
 
-@app.route('/rate', methods=['POST', 'GET'])
-def rate():
-	data = request.get_json()
-	found = mongo.db.movie.find_one({'id' : data.id})	
-	if found :
-		found.rating.value = (found.rating.value * found.rating.count + data.rating)/(found.rating.count + 1)
-		found.rating.count = found.rating.count + 1
-	else :
-		mongo.db.movie.insert_one({'id' : data.id, 'rating' : {data.rating, 1}, 'comments' : []})
-	return mongo.db.movie.find_one({'id' : data.id})
+# @app.route('/rate', methods=['POST', 'GET'])
+# def rate():
+# 	data = request.get_json()
+# 	found = mongo.db.movie.find_one({'id' : data.id})
+# 	if found :
+# 		found.rating.value = (found.rating.value * found.rating.count + data.rating)/(found.rating.count + 1)
+# 		found.rating.count = found.rating.count + 1
+# 	else :
+# 		mongo.db.movie.insert_one({'id' : data.id, 'rating' : {data.rating, 1}, 'comments' : []})
+# 	return mongo.db.movie.find_one({'id' : data.id})
 
-@app.route('/history', methods=['POST', 'GET'])
-def history():
-	data = request.get_json()
-	user = mongo.db.user.find_one({'userID' : data.userID})
-	user.movies.append(data.movie)
+# @app.route('/history', methods=['POST', 'GET'])
+# def history():
+# 	data = request.get_json()
+# 	user = mongo.db.user.find_one({'userID' : data.userID})
+# 	user.movies.append(data.movie)
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,11 +1,62 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
 import { GoogleLogout } from "react-google-login";
+import URL from "./Url";
+import axios from "axios";
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = [];
     this.history = this.props.history;
+    this.state.searchText = "";
+    this.searchMovie = this.searchMovie.bind(this);
+    this.state.searchedMovieList = [];
+  }
+  searchMovie() {
+    console.log("Hello");
+    axios
+      .post(URL + "search/" + this.state.searchText)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          searchedMovieList: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  renderSearchedMovieList() {
+    return (
+      <div>
+        {this.state.searchedMovieList.map((movie, index) => {
+          <div>
+            <p>{movie.l}</p>
+            <img src={movie.i.imageURL} />
+          </div>;
+        })}
+      </div>
+    );
+  }
+  renderSearchBar() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            id="Movie"
+            name="Movie"
+            value={this.state.searchText}
+            onChange={(e) => {
+              this.setState({
+                searchText: e.target.value,
+              });
+            }}
+          />
+        </form>
+        <button onClick={this.searchMovie}>Search</button>
+      </div>
+    );
   }
   render() {
     return (
@@ -15,12 +66,15 @@ class Home extends React.Component {
           clientId="427815533001-pfdvja4nu5kc7dp33j48b7c2e9vcvvta.apps.googleusercontent.com"
           buttonText="Logout"
           onLogoutSuccess={(response) => {
+            //localStorage.clear();
             this.history.goBack();
           }}
           onFailure={(response) => {
             console.log(response);
           }}
         ></GoogleLogout>
+        {this.renderSearchBar()}
+        {this.renderSearchedMovieList()}
       </div>
     );
   }
